b02aeca457c7ad66476fbfd4a904ed110affab78
Wilmo95 <bartosz.wilmowicz@gmail.com>
2020-11-23 21:36:59 +0100

    Added base version of maze.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from pyeasyga import pyeasyga
 import numpy as np
 
-def maze(size = None):
-    np.
+
+def createMaze(size = None):
+        maze = []
+        maze.append([1,1,1,1,1,1,1,1,1,1,1,1])
+        maze.append([1,1,0,0,1,0,0,0,1,0,0,1])
+        maze.append([1,1,1,0,0,0,1,0,1,1,0,1])
+        maze.append([1,0,0,0,1,0,1,0,0,0,0,1])
+        maze.append([1,0,1,0,1,1,0,0,1,1,0,1])
+        maze.append([1,0,0,1,1,0,0,0,1,0,0,1])
+        maze.append([1,0,0,0,0,0,1,0,0,0,1,1])
+        maze.append([1,0,1,0,0,1,1,0,1,0,0,1])
+        maze.append([1,0,1,1,1,0,0,0,1,1,0,1])
+        maze.append([1,0,1,0,1,1,0,1,0,1,0,1])
+        maze.append([1,0,1,0,0,0,0,0,0,0,'E',1])
+        maze.append([1,1,1,1,1,1,1,1,1,1,1,1])
+
+        return maze
+
+maze = createMaze()
