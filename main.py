fd7593b782bece29d0701dcf19dc508fc50de39c
Wilmo95 <bartosz.wilmowicz@gmail.com>
2020-11-25 22:51:14 +0100

    Added function 'glue'

--- main.py
+++ main.py
@@ -1,45 +1,53 @@
 from pyeasyga import pyeasyga
 import numpy as np
 
-data_no = [{'direction': 'left'},
-        {'direction': 'right'},
-        {'direction': 'up' },
-        {'direction': 'down' }]
+how_many_moves = 40
+data_no = [{'bit': '1'}]
 data = []
-for i in range(10):
-        data.append(data_no[0])
-        data.append(data_no[1])
-        data.append(data_no[2])
-        data.append(data_no[3])
-print(data)
-def createMaze(size = None):
-        maze = []
-        maze.append([1,1,1,1,1,1,1,1,1,1,1,1])
-        maze.append([1,'S',0,0,1,0,0,0,1,0,0,1])
-        maze.append([1,1,1,0,0,0,1,0,1,1,0,1])
-        maze.append([1,0,0,0,1,0,1,0,0,0,0,1])
-        maze.append([1,0,1,0,1,1,0,0,1,1,0,1])
-        maze.append([1,0,0,1,1,0,0,0,1,0,0,1])
-        maze.append([1,0,0,0,0,0,1,0,0,0,1,1])
-        maze.append([1,0,1,0,0,1,1,0,1,0,0,1])
-        maze.append([1,0,1,1,1,0,0,0,1,1,0,1])
-        maze.append([1,0,1,0,1,1,0,1,0,1,0,1])
-        maze.append([1,0,1,0,0,0,0,0,0,0,'E',1])
-        maze.append([1,1,1,1,1,1,1,1,1,1,1,1])
+for i in range(how_many_moves*2):
+    data.append(data_no[0])
 
-        return maze
+
+def create_maze(size = None):
+    maze = []
+    maze.append(['#','#','#','#','#','#','#','#','#','#','#','#'])
+    maze.append(['#','S',' ','#',' ',' ',' ','#',' ',' ',' ','#'])
+    maze.append(['#','#','#',' ',' ',' ','#',' ','#','#',' ','#',])
+    maze.append(['#',' ',' ',' ','#',' ','#',' ',' ',' ',' ','#',])
+    maze.append(['#',' ','#',' ','#','#',' ',' ','#','#',' ','#',])
+    maze.append(['#',' ',' ','#','#',' ',' ',' ','#',' ',' ','#',])
+    maze.append(['#',' ',' ',' ',' ',' ','#',' ',' ',' ','#','#',])
+    maze.append(['#',' ','#',' ',' ','#','#',' ','#',' ',' ','#',])
+    maze.append(['#',' ','#','#','#',' ',' ',' ','#','#',' ','#',])
+    maze.append(['#',' ','#',' ','#','#',' ','#',' ','#',' ','#',])
+    maze.append(['#',' ','#',' ',' ',' ',' ',' ',' ',' ','E','#'])
+    maze.append(['#','#','#','#','#','#','#','#','#','#','#','#'])
+
+    return maze
+def glue(chosen, bits):
+    moves_temp = []
+    moves = []
+    for i in chosen:
+        if i:
+            moves_temp.append(bits[0].get('bit'))
+        else:
+            moves_temp.append('0')
+    for ele in range(0, len(moves_temp), 2):
+        moves.append(moves_temp[ele] + moves_temp[ele+1])
+
+    return moves
+
 
 population = 200
 mutation = 0.05
 alg = pyeasyga.GeneticAlgorithm(data, population_size= population, mutation_probability= mutation, elitism= True)
-maze = createMaze()
+maze = create_maze()
 
-def fitness(item , data):
-        distance = None
-        for chosen, dir in zip(item, data):
-                if chosen:
-                       move = dir.get('direction')
-                       if move == 'left'
+def fitness(item, data):
+    print(data)
+    moves = glue(item, data)
+    print(moves)
+
 
 # class new_ga(pyeasyga.GeneticAlgorithm):
 alg.fitness_function = fitness
